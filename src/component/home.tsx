e453167fbe5193d16a8d57f7f5df0f5e06944432
workuMekuriya <mekuriya185@gmail.com>
2025-03-24 14:20:08 +0300

    api test

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,28 +1,45 @@
-import React from "react";
-import FeaturesBox from "./featureBox";
-import HomePageForm from "./quoetForm";
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import FeaturesBox from './featureBox'
+import HomePageForm from './quoetForm'
 
 export interface IHomeProps {}
 
-export default function Home(props: IHomeProps) {
+export default function Home (props: IHomeProps) {
+  const [quotes, setQuotes] = useState([])
+
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      try {
+        const response = await axios.get('https://146.190.145.241/quotes')
+        setQuotes(response.data)
+      } catch (error) {
+        console.error('Error fetching quotes:', error)
+      }
+    }
+
+    fetchQuotes()
+  }, [])
+
   return (
-    <section id="home">
+    <section id='home'>
       <div
-        id="section-1"
-        className="relative block w-full h-[762px] mx-auto bg-white bg-opacity-70  bg-center bg-cover bg-no-repeat"
+        id='section-1'
+        className='relative block w-full h-[762px] mx-auto bg-white bg-opacity-70 bg-center bg-cover bg-no-repeat'
       >
         {/* Content */}
-        <div className="relative z-10 flex flex-col md:flex-row items-center justify-start md:justify-between py-16 px-4 h-full">
+        <div className='relative z-10 flex flex-col md:flex-row items-center justify-start md:justify-between py-16 px-4 h-full'>
           {/* Left Section */}
-          <div className="text-gray-900 max-w-[600px]">
-            <div id="lp-pom-text-481" className="mb-3 px-6 pt-20 pb-10 md:py-6">
-              <p className="text-[35px] md:text-[52px] font-extrabold leading-[50px]  md:leading-[77px] font-montserrat text-left md:text-left">
-                {/* enclosed or open, always professional.  */}
-                ENCLOSED OR OPEN AUTO TRANSPORT{" "}
+          <div className='text-gray-900 max-w-[600px]'>
+            <div id='lp-pom-text-481' className='mb-3 px-6 pt-20 pb-10 md:py-6'>
+              <p className='text-[35px] md:text-[52px] font-extrabold leading-[50px] md:leading-[77px] font-montserrat text-left md:text-left'>
+                ENCLOSED OR OPEN AUTO TRANSPORT
               </p>
             </div>
-            <div id="lp-pom-text-482" className="mt-2 px-6">
-              <p className="text-[20px] md:text-[20px] font-base leading-[29px] font-montserrat text-left">
+            <div id='lp-pom-text-482' className='mt-2 px-6'>
+              <p className='text-[20px] md:text-[20px] font-base leading-[29px] font-montserrat text-left'>
                 Safely transporting all personnel, business, classic and exotic
                 vehicles for individuals, dealerships, and auction houses with
                 care and reliability.
