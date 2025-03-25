51cd571b0dd68595ec6470b0f8913c142dd555ea
freedoom16 <nmaaa007@gmail.com>
2025-03-25 16:27:39 +0300

    update

--- src/component/testimonials.tsx
+++ src/component/testimonials.tsx
@@ -5,54 +5,55 @@ const Testimonials = () => {
   const testimonials = [
     {
       id: 1,
-      name: "Alice Johnson",
-      position: "Classic Car Enthusiast",
-      image: "https://placeimg.com/150/150/people",
-      title: "Outstanding Enclosed Transport",
-      description:
-        "Their enclosed transport services are fantastic! After dealing with multiple companies that failed to meet my expectations or caused unnecessary delays, this one stood out. The cost was slightly higher than some quotes, but it was completely worth it—no hidden fees or surprises",
-    },
-    {
-      id: 2,
-      name: "Michael Smith",
-      position: "Satisfied Customer",
-      image: "https://placeimg.com/150/150/people",
-      title: "Seamless Open Transport Service",
-      description:
-        "This company delivered on its promises with open transport! The price was transparent and reasonable. Communication was amazing—I received updates via text and email, and the driver handled my car with care. Everything went smoothly, and my vehicle arrived without a scratch. Highly recommend!",
-    },
-    {
-      id: 3,
-      name: "Sophia Williams",
+      name: "Mekuriya Worku",
       position: "Frequent Mover",
-      image: "https://placeimg.com/150/150/people",
+      image: "/review/review6.jpg",
       title: "Reliable and Stress-Free",
       description:
         "I used their services for an interstate move, and it was stress-free from start to finish. The team kept me informed throughout, and the driver was very professional. My car arrived on time and in great condition. It’s hard to find such reliable service these days!",
     },
     {
-      id: 4,
-      name: "James Carter",
+      id: 2,
+      name: "Gabriela Fasil",
       position: "Business Owner",
-      image: "https://placeimg.com/150/150/people",
+      image: "/review/review7.jpg",
       title: "Great Experience with Open Transport",
       description:
         "I had my fleet vehicle transported using their open transport service, and it was a great experience. The driver was punctual and followed all my instructions. The entire process was smooth, and my car arrived safely. Excellent service overall!",
     },
     {
-      id: 5,
-      name: "Emma Brown",
-      position: "Car Enthusiast",
-      image: "https://placeimg.com/150/150/people",
+      id: 3,
+      name: "John Paul",
+      position: "Classic Car Enthusiast",
+      image: "/review/review5.png",
+      title: "Outstanding Enclosed Transport",
+      description:
+        "Their enclosed transport services are fantastic! After dealing with multiple companies that failed to meet my expectations or caused unnecessary delays, this one stood out. The cost was slightly higher than some quotes, but it was completely worth it—no hidden fees or surprises",
+    },
+    {
+      id: 4,
+      name: "Lali Kalab",
+      position: "First-Time User",
+      image: "/review/review8.png",
       title: "Professional Enclosed Transport",
       description:
         "Their enclosed transport is top-notch! My car was handled with care, and the team provided regular updates. The driver was courteous and made sure everything was secure. I’ll definitely use their services again for my luxury cars!",
     },
     {
+      id: 5,
+      name: "Netsanet moss",
+      position: "Satisfied Customer",
+      image: "/review/review4.png",
+      title: "Seamless Open Transport Service",
+      description:
+        "This company delivered on its promises with open transport! The price was transparent and reasonable. Communication was amazing—I received updates via text and email, and the driver handled my car with care. Highly recommend!",
+    },
+
+    {
       id: 6,
-      name: "David Wilson",
-      position: "First-Time User",
-      image: "https://placeimg.com/150/150/people",
+      name: "Fillimon B",
+      position: "Car Enthusiast",
+      image: "/review/review2.png",
       title: "Excellent Communication and Service",
       description:
         "As a first-time user, I was nervous about transporting my car, but they made it easy. The team was responsive, and I appreciated the updates during transit. My vehicle arrived earlier than expected, in perfect condition. Highly recommend!",
@@ -117,14 +118,16 @@ const Testimonials = () => {
                   className={`flex-none ${isDesktop ? "w-1/3" : "w-full"} px-2`}
                 >
                   <div
-                    className="p-6 md:p-12 rounded-lg border bg-white border-gray-200 mb-8 text-gray-900"
+                    className="p-6  md:p-12 rounded-lg border bg-white border-gray-200 mb-8 text-gray-900 flex flex-col h-full"
                     style={{ boxShadow: "0 10px 28px rgba(0,0,0,.08)" }}
                   >
                     <p className="text-xl font-semibold">{testimonial.title}</p>
-                    <p className="mt-4 p-2 flex">{testimonial.description}</p>
-                    <div className="flex items-center mt-8">
+                    <p className="mt-4 p-2 flex-grow ">
+                      {testimonial.description}
+                    </p>
+                    <div className="flex items-center mt-2 lg:mt-6">
                       <img
-                        className="w-12 h-12 mr-4 rounded-full"
+                        className="w-20 h-20 mr-2  rounded-full"
                         src={testimonial.image}
                         alt={testimonial.name}
                       />
