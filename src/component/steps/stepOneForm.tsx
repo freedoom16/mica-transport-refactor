3660d48b1ffef080fae40167fcabb2feebf6d94f
freedoom16 <nmaaa007@gmail.com>
2025-03-26 20:30:00 +0300

    update

--- src/component/steps/stepOneForm.tsx
+++ src/component/steps/stepOneForm.tsx
@@ -805,8 +805,8 @@ const StepOne: React.FC<StepOneProps> = ({
         </div>
       </div>
 
-      {currentVehicleIndex > 1 &&
-        currentLocationIndex < currentVehicleIndex - 1 && (
+      {currentVehicleIndex >= 1 &&
+        currentLocationIndex <= currentVehicleIndex - 1 && (
           <button
             type="button"
             className="bg-white text-[#6DB8D1] border-2 border-[#6DB8D1] font-bold py-2 px-4 rounded-full"
